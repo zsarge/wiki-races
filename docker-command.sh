efcd79e7fbc5dcd10b1b1a72311d1e880181ccea
zsarge <zackarywsargent@gmail.com>
2021-07-02 23:06:35 -0400

    temp: attempt docker fix

--- docker-command.sh
+++ docker-command.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-docker build -t my-golang-app . && docker run --net="host" my-golang-app:latest
+# docker build -t my-golang-app . && docker run --net="host" my-golang-app:latest
+docker build -t wiki-races . && docker run -it -p 8080:80 wiki-races
