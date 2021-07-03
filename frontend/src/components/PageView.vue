195ea57638b0486053c0a7e2b24af142c61c51f2
zsarge <zackarywsargent@gmail.com>
2021-07-03 16:13:43 -0400

    Add page view
    The page view will handle loading and dealing with the wikipedia pages.
    I will add other controls around it.

---
+++ frontend/src/components/PageView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="PageView">This is the wikipedia content</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "PageView",
+});
+</script>
--- frontend/src/views/Game.vue
+++ frontend/src/views/Game.vue
@@ -1,1 +1,18 @@
-<template>oh yeah gamer time</template>
+<template>
+  <div class="game">
+    This is the view of the page:
+    <PageView />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import PageView from "@/components/PageView.vue";
+
+export default defineComponent({
+  name: "Game",
+  components: {
+    PageView,
+  },
+});
+</script>
